47d21c0d9744660f0dc8c9b54a325d2bbea545e2
Chin Zheng Hao <chinhao61@gmail.com>
2022-07-21 22:32:00 +0700

    shti finna works

--- run_bob_base_docker_container.sh
+++ run_bob_base_docker_container.sh
@@ -2,8 +2,10 @@
 
 xhost +
 
-docker run -it --rm --net=host \
+docker run -it --rm \
     --env="DISPLAY" \
     --env="QT_X11_NO_MITSHM=1" \
+    --privileged \
+    --volume="/dev/bus/usb:/dev/bus/usb" \
     --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
     bob_base
